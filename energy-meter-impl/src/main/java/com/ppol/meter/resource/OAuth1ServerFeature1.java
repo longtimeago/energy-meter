03284704d37add8df8fb67dfca0c3789510652d8
Paul Polishchuk <lngtmg@gmail.com>
2024-10-11 19:02:30 +0300

    remove impl

--- energy-meter-impl/src/main/java/com/ppol/meter/resource/OAuth1ServerFeature1.java
+++
@@ -1,8 +0,0 @@
-package com.ppol.meter.resource;
-
-import javax.ws.rs.ext.Provider;
-import org.glassfish.jersey.server.oauth1.OAuth1ServerFeature;
-
-//@Provider
-public class OAuth1ServerFeature1 extends OAuth1ServerFeature {
-}
--- energy-meter-impl/src/main/java/com/ppol/meter/resource/RolesAllowedDynamicFeature1.java
+++
@@ -1,8 +0,0 @@
-package com.ppol.meter.resource;
-
-import javax.ws.rs.ext.Provider;
-import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
-
-@Provider
-public class RolesAllowedDynamicFeature1 extends RolesAllowedDynamicFeature {
-}
